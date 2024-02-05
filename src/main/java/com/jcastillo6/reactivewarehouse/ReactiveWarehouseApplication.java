31e8ec4a4d65845a2aec0f99b18b9b605690df7b
jcastilo6 <castillo.guerra@gmail.com>
2024-02-05 09:08:29 +0100

    improvements

--- src/main/java/com/jcastillo6/reactivewarehouse/ReactiveWarehouseApplication.java
+++ src/main/java/com/jcastillo6/reactivewarehouse/ReactiveWarehouseApplication.java
@@ -2,12 +2,47 @@ package com.jcastillo6.reactivewarehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Hooks;
+
+@Slf4j
 @SpringBootApplication
 public class ReactiveWarehouseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReactiveWarehouseApplication.class, args);
+        Hooks.enableAutomaticContextPropagation();
+    }
+
+
+    @Bean
+    ApplicationListener<ApplicationStartedEvent> doOnStart(ObservationRegistry registry) {
+        return event -> generateString(registry);
+    }
+
+    public void generateString(ObservationRegistry registry) {
+        String something = Observation
+            .createNotStarted("server.job", registry)	//1
+            .lowCardinalityKeyValue("jobType", "string")	//2
+            //3
+            .observe(() -> {
+                log.info("Generating a String...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                    return "NOTHING";
+                }
+                return "SOMETHING";
+            });
+
+        log.info("Result was: " + something);
     }
 
 }
--- src/main/java/com/jcastillo6/reactivewarehouse/service/WarehouseService.java
+++ src/main/java/com/jcastillo6/reactivewarehouse/service/WarehouseService.java
@@ -1,17 +1,28 @@
 package com.jcastillo6.reactivewarehouse.service;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
 import java.util.UUID;
+import java.util.function.LongSupplier;
 import org.springframework.stereotype.Service;
 import com.jcastillo.warehouse.model.Warehouse;
 import com.jcastillo6.reactivewarehouse.entity.WarehouseEntity;
 import com.jcastillo6.reactivewarehouse.repository.WarehouseRepo;
+
+import io.micrometer.observation.annotation.Observed;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
+@Slf4j
 public class WarehouseService implements AbstractService<WarehouseEntity>
 {
+    private static final Random random = new Random();
+    private static final LongSupplier latency = () -> random.nextLong(500);
     private final WarehouseRepo warehouseRepo;
+
 
     public WarehouseService(WarehouseRepo warehouseRepo) {
         this.warehouseRepo = warehouseRepo;
@@ -36,11 +47,22 @@ public class WarehouseService implements AbstractService<WarehouseEntity>
         return warehouseRepo.save(entity);
     }
 
+    @Observed(name = "save.warehouse",
+        contextualName = "saving-warehouse",
+        lowCardinalityKeyValues = {"type", "address"})
     public Mono<WarehouseEntity> save(Mono<Warehouse> warehouse) {
-        return warehouse.map(this::toWarehouseEntity).flatMap(this::save);
+        log.info("Validating warehouse");
+
+        return warehouse.map(this::toWarehouseEntity)
+            .flatMap(this::save)
+            .delayElement(Duration.of(latency.getAsLong(), ChronoUnit.MILLIS));
     }
 
-    private WarehouseEntity toWarehouseEntity(Warehouse warehouse) {
+    @Observed(name = "mapping.warehouse",
+        contextualName = "mapping-warehouse",
+        lowCardinalityKeyValues = {"type", "address"})
+    public WarehouseEntity toWarehouseEntity(Warehouse warehouse) {
+        log.info("mapping warehouse");
         var warehouseEntity = new WarehouseEntity();
         warehouseEntity.setName(warehouse.getName());
         warehouseEntity.setId(UUID.randomUUID());
